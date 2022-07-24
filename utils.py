eb6dd0dca64075943e2e7ba88e0a32d304632fd4
dongwook <dwchung95@gmail.com>
2022-07-24 16:19:47 +0900

    refactor function: cocnat_csv_files_in_dir()

--- utils.py
+++ utils.py
@@ -5,7 +5,32 @@ import pandas as pd
 from tqdm import tqdm
 from datetime import datetime
 
+def concat_csv_files_in_dir(get_dirpath, put_fpath):
+    df = []
+    csv_file_generator = (pd.read_csv(os.path.join(get_dirpath, csv_fname)) for csv_fname in os.listdir(get_dirpath) if csv_fname.endswith('csv'))
+    for csv_file in csv_file_generator:
+        df.append(csv_file)
+    df = pd.concat(df)
+    df.to_csv(put_fpath, index=False, encoding='utf-8-sig')
 
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = datetime.now()
+        str_start_time = start_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
+        print(f"Process [{func.__name__}] Started at : {str_start_time}")
+
+        result = func(*args, **kwargs)
+        
+        end_time = datetime.now()
+        str_end_time = end_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
+        interval = end_time - start_time
+        str_interval = str(np.round(interval.total_seconds(), 3))
+        print(f"Process [{func.__name__}] Ended at   : {str_end_time}")
+        print(f"Process [{func.__name__}] Runned for : {str_interval} sec")
+        print()
+
+        return result
+    return wrapper
 
 def get_memory_usage(df):
     # compare with df.info(memory_usage='deep')
@@ -32,33 +57,3 @@ def downcast_df(df, apply_int=True, apply_float=True, apply_string=False, print_
     if print_size:
         print(f"DataFrame Downcasted: {before} -> {after}")
     return df_copied
-
-def concat_csv_files_in_dir(get_dir, put_dir, fname):
-    df = []
-    csv_file_generator = (pd.read_csv(os.path.join(get_dir, csv_fname)) for csv_fname in os.listdir(get_dir) if csv_fname.endswith('csv'))
-    for csv_file in csv_file_generator:
-        df.append(csv_file)
-    df = pd.concat(df)
-    df.to_csv(os.path.join(put_dir, fname), index=False, encoding='utf-8-sig')
-
-
-def measure_time(func):
-    def wrapper(*args, **kwargs):
-        start_time = datetime.now()
-        str_start_time = start_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
-        print(f"Process [{func.__name__}] Started at : {str_start_time}")
-
-        result = func(*args, **kwargs)
-        
-        end_time = datetime.now()
-        str_end_time = end_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
-        interval = end_time - start_time
-        str_interval = str(np.round(interval.total_seconds(), 3))
-        print(f"Process [{func.__name__}] Ended at   : {str_end_time}")
-        print(f"Process [{func.__name__}] Runned for : {str_interval} sec")
-        print()
-
-        return result
-    return wrapper
-
-# def 함수 실행의 시작과 끝을 알려주는 데코레이터
