2767562b1643b5f4d82db6f4ecf4258674ef4655
dongwook <dwchung95@gmail.com>
2022-09-07 00:32:44 +0900

    [feat] upload_history_to_bq()

--- bq_uploader.py
+++ bq_uploader.py
@@ -47,9 +47,44 @@ class BqUploader(DirectoryBuilder):
             print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
             print()
 
+    def upload_history_to_bq(self):
+        client = bigquery.Client(project=self.bq_project_id)
+        table_id = f"{self.bq_project_id}.{self.bq_dataset_id}.{self.bq_table_id_history}"
+        client.create_table(table_id, exists_ok=True)
+
+        dirpath = self.dirpath_history_pp_concatenated
+        history_generator = (pd.read_csv(os.path.join(dirpath, x)) for x in tqdm(os.listdir(dirpath)) if x.endswith('csv'))
+        for i, summary in enumerate(history_generator):
+            get_write_disposition = lambda i: 'WRITE_TRUNCATE' if (i==0) else 'WRITE_APPEND'                            
+            print(f'Row, Col:{summary.shape} | Write-Disposition: {get_write_disposition(i)}')
+
+            job_config = bigquery.LoadJobConfig(
+                schema=[
+                    bigquery.SchemaField("open", "FLOAT64"),
+                    bigquery.SchemaField("high", "FLOAT64"),
+                    bigquery.SchemaField("low", "FLOAT64"),
+                    bigquery.SchemaField("close", "FLOAT64"),                    
+                    bigquery.SchemaField("stock_split", "FLOAT64"),
+                    bigquery.SchemaField("dividend", "FLOAT64"),
+                    bigquery.SchemaField("dividend_paid_or_not", "FLOAT64"),
+                    bigquery.SchemaField("volume", "FLOAT64"),
+                    bigquery.SchemaField("volume_of_dollar", "FLOAT64"),
+                    bigquery.SchemaField("volume_of_share", "FLOAT64")
+                ],
+                write_disposition=get_write_disposition(i)
+            )
+            job = client.load_table_from_dataframe(summary, table_id, job_config=job_config)
+            job.result()  # Wait for the job to complete.
+            table = client.get_table(table_id)  # Make an API request.
+            print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
+
+    def check_table(self):
+        pass
+
 if __name__ == "__main__":
     if 'stock-database-builder' in os.listdir():
         os.chdir('stock-database-builder')
 
     bq_uploader = BqUploader()
-    bq_uploader.upload_summary_to_bq()
+    # bq_uploader.upload_summary_to_bq()
+    bq_uploader.upload_history_to_bq()
