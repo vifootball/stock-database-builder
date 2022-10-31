923935b8523f1d5ef76a180cab33b7e764587e16
dongwook <dwchung95@gmail.com>
2022-11-01 08:55:20 +0900

    edit save_dfs_by_chunk()

--- preprocessor.py
+++ preprocessor.py
@@ -50,27 +50,29 @@ class Preprocessor():
         df_list = []
         total_len = 0
         chunk_num = 1
-        put_fpath = os.path.join(put_dirpath, f'{prefix_chunk}_{chunk_num}.csv')
 
         df_generator =  (pd.read_csv(os.path.join(get_dirpath, fname)) for fname in os.listdir(get_dirpath) if fname.endswith('csv'))
         for df in df_generator:
             df_list.append(df)
             total_len += len(df)
-
             print(f'Concatenateing Dfs in {get_dirpath} | Chunk No.{chunk_num} | Total Length: {total_len}')
+            
             if total_len > 1000_000:
                 df_concatenated = pd.concat(df_list)
-                df_concatenated.to_csv(put_fpath, index=False)
+                export_df_to_csv(
+                    df=df_concatenated,
+                    fpath=os.path.join(put_dirpath, f'{prefix_chunk}_{chunk_num}.csv')
+                )
 
                 df_list = []
                 total_len = 0
                 chunk_num += 1
         
         if total_len > 0: # 나눠 떨어지지 않은 마지막 사이클 저장
-            df = pd.concat(df_list)
+            df_concatenated = pd.concat(df_list)
             export_df_to_csv(
-                df = df,
-                fpath=put_fpath
+                df = df_concatenated,
+                fpath=os.path.join(put_dirpath, f'{prefix_chunk}_{chunk_num}.csv')
             )
 
 
