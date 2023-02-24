42595c56df3869a3739a7110dc42ca669017f124
Chung Dongwook <chungdongwook@Chungs-MacBook-Pro.local>
2023-02-24 10:29:06 +0900

    initial commit

---
+++ new_constants.py
@@ -0,0 +1,40 @@
+class AssetCategories():
+    EQUITY = [
+        "Large Blend", "Large Value", "Technology", "Miscellaneous Region", "Large Growth",
+        "Foreign Large Blend", "Diversified Emerging Mkts", "Small Blend", "Mid-Cap Blend","Health", "Natural Resources",
+        "China Region", "Financial", "Foreign Large Value", "Mid-Cap Growth", "Consumer Cyclical", 
+        "Mid-Cap Value", "Equity Energy", "Miscellaneous Sector", "Small Value", "Europe Stock",
+        "Industrials", "Energy Limited Partnership","Small Growth", "Foreign Large Growth", "Equity Precious Metals",
+        "Consumer Defensive", "Long-Short Equity", "Communications", "Utilities", "Pacific/Asia ex-Japan Stk", 
+        "Preferred Stock", "India Equity","Foreign Small/Mid Blend", "Foreign Small/Mid Value", "Infrastructure",
+        "Latin America Stock","World Stock", "Diversified Pacific/Asia", "Foreign Small/Mid Growth", "Bear Market",
+        "Market Neutral", "Multialternative", "Long-Short Credit", "Japan Stock"
+    ]
+    BOND = [
+        "High Yield Bond", "Corporate Bond", "Ultrashort Bond", "Short-Term Bond", "Intermediate-Term Bond",
+        "Emerging Markets Bond", "Muni National Interm", "Multisector Bond", "Intermediate Government","Inflation-Protected Bond",
+        "Long Government", "World Bond", "Nontraditional Bond", "Emerging-Markets Local-Currency Bond", "Short Government",
+        "Bank Loan", "Muni National Long", "Muni National Short", "Long-Term Bond", "High Yield Muni",
+        "Convertibles", "Muni Minnesota", "Muni New York Intermediate", "Muni California Long"
+    ]
+    COMMODITY = [
+        "Commodities Broad Basket"
+    ]
+    OTHER = [
+        "Trading--Leveraged Equity", "Trading--Inverse Equity","Trading--Miscellaneous",
+        "Trading--Inverse Debt", "Trading--Leveraged Commodities", "Trading--Inverse Commodities", "Trading--Leveraged Debt",
+        "Real Estate", "Global Real Estate",
+        "World Allocation", "Tactical Allocation",
+        "Volatility", "Single Currency", "Managed Futures", "Option Writing", "Multicurrency",
+        r"Allocation--15% to 30% Equity",
+        r"Allocation--30% to 50% Equity",
+        r"Allocation--50% to 70% Equity",
+        r"Allocation--70% to 85% Equity",
+        r"Allocation--85%+ Equity"
+    ]
+
+
+class FredSymbols():
+    SYMBOLS = [
+        
+    ]
