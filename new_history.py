5b959efacec489c1cdf343c052761a591a307bc8
Chung Dongwook <chungdongwook@Chungs-MacBook-Pro.local>
2023-02-26 09:15:55 +0900

    initial commit

---
+++ new_history.py
@@ -0,0 +1,69 @@
+from typing import Union
+import pandas as pd
+import pandas_datareader.data as web
+import yfinance as yf
+import datetime as dt
+from new_table import TableHandler
+import new_table_config
+
+class History:
+    def __init__(self):
+        self.src_history_table_handler = TableHandler(table_config=new_table_config.SRC_HISTORY)
+        self.trg_history_table_handler = TableHandler(table_config=new_table_config.TRG_HISTORY)
+
+    def get_history_from_yf(self, symbol: str) -> Union[pd.DataFrame, None]:
+        # get raw data    
+        history = yf.Ticker(symbol).history(period='max').reset_index(drop=False) # date가 index
+        # 데이터가 어느정도 있으면서 최근까지 업데이트 되는 종목 # 아예 없거나 과거 데이터만 있거나
+        if len(history) > 50:
+            today = dt.datetime.today()
+            last_traded_day = history['Date'].max().replace(tzinfo=None)
+            days_from_last_traded = (today - last_traded_day)
+            if days_from_last_traded < pd.Timedelta('50 days'):
+                # table handling
+                history['symbol'] = symbol.upper()
+                table_handler = self.src_history_table_handler
+                history = table_handler.rename_columns(history)
+                history = table_handler.select_columns(history)
+                history['date'] = history['date'].dt.strftime('%Y-%m-%d')
+        else:
+            history = None
+
+        return history
+
+    def get_history_from_fred(self, symbol: str) -> Union[pd.DataFrame, None]:
+        try:
+            # get raw data
+            start, end = (dt.datetime(1800, 1, 1), dt.datetime.today())
+            history = web.DataReader(symbol, 'fred', start, end)#.asfreq(freq='1d', method='ffill').reset_index(drop=False)
+            history = history.reset_index()
+            history.rename(columns={
+                f'{symbol}': 'close',
+                'DATE': 'date'
+            }, inplace=True)
+            history['symbol'] = symbol.upper()
+            history['date'] = history['date'].dt.strftime('%Y-%m-%d')
+
+            # table handling
+            table_handler = self.src_history_table_handler
+            header = pd.DataFrame(columns=table_handler.get_columns_to_select())
+            history = pd.concat([header, history], axis=0)
+    
+        except:
+            history = None
+
+        return history
+
+    def transform_history(history: pd.DataFrame) -> pd.DataFrame:
+        if history is None:
+            return None
+        else:
+            _cal_1()
+            _cal_2()
+        pass
+
+    def _cal_1():
+        pass
+
+    def _cal_2():
+        pass
