e3fe83d2bf99f4c438b2db6071ffda0230bb38db
Chung Dongwook <chungdongwook@Chungs-MacBook-Pro.local>
2023-02-27 16:36:26 +0900

    initial commit

---
+++ new_config.py
@@ -0,0 +1,25 @@
+import os
+
+# bigquery
+BQ_PROJECT_ID = 'between-buy-and-sell'
+BQ_DATASET_ID = 'stock'
+BQ_TABLE_ID_SUMMARY = 'summary'
+BQ_TABLE_ID_HISTORY = 'history'
+
+# directories
+DIR_BASE = os.path.dirname(os.path.abspath(__file__))
+DIR_DOWNLOAD = os.path.join(DIR_BASE, 'download')
+
+DIR_METADATA_ETF = os.path.join(DIR_DOWNLOAD, 'metadata', 'etf')
+DIR_METADATA_CURRENCY = os.path.join(DIR_DOWNLOAD, 'metadata', 'currency')
+DIR_METADATA_INDICES = os.path.join(DIR_DOWNLOAD, 'metadata', 'indices')
+DIR_METADATA_CHUNK = os.path.join(DIR_DOWNLOAD, 'metadata', 'chunk')
+
+DIR_HISTORY_ETF = os.path.join(DIR_DOWNLOAD, 'history', 'etf')
+DIR_HISTORY_CHURRENCY = os.path.join(DIR_DOWNLOAD, 'history', 'currency')
+DIR_METADATA_INDICES = os.path.join(DIR_DOWNLOAD, 'history', 'indices')
+DIR_METADATA_CHUNK = os.path.join(DIR_DOWNLOAD, 'history', 'chunk')
+
+DIR_DATEDIM = os.path.join(DIR_DOWNLOAD, 'date_dim')
+
+# DIR_SUMMARY
