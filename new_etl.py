503debebdb6f337473b668ec52c8184f4482d863
Chung Dongwook <chungdongwook@Chungs-MacBook-Pro.local>
2023-02-28 14:08:28 +0900

    initial commit

---
+++ new_etl.py
@@ -0,0 +1,144 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+from new_utils import *
+import new_config
+from new_indices import Indices
+from new_currency import Currency
+from new_metadata_collector import ETF
+from new_history import History
+from new_date_dim import DateDim
+
+# def etl_metadata():
+#     _etl_metadata_etf()
+#     _etl_metadata_indices()
+#     _etl_metadata_currencies()
+
+#     for metdata in metadata_dir:
+#         load_to_bigquery
+
+
+def etl_metadata_etf():
+    etf = ETF()
+    symbols = etf.get_symbols()
+
+    dirpath_metadata = new_config.DIR_METADATA_ETF
+    os.makedirs(dirpath_metadata, exist_ok=True)
+
+    for symbol in tqdm(symbols[:], mininterval=0.5):
+        metadata = etf.get_metadata(symbol=symbol)
+        fname = os.path.join(dirpath_metadata, f"metadata_etf_{symbol}.csv")
+        fpath = os.path.join(dirpath_metadata, fname)
+        metadata.to_csv(fpath, index=False)
+    
+    dirpath_metadata_chunk = new_config.DIR_METADATA_CHUNK
+    os.makedirs(dirpath_metadata_chunk, exist_ok=True)
+    fname = 'metadata_etf_chunk.csv'
+    fpath = os.path.join(dirpath_metadata_chunk, fname)
+    metadata_chunk = concat_csv_files_in_dir(get_dirpath = new_config.DIR_METADATA_ETF)
+    metadata_chunk.to_csv(fpath, index=False)
+    
+
+def etl_metadata_indices():
+    indices = Indices()
+    metadata_from_fd = indices.get_metadata_from_fd()
+    metadata_from_investpy = indices.get_metadata_from_investpy()
+    metadata_from_yahoo_main = indices.get_metadata_from_yahoo_main()
+    metadata_from_fred = indices.get_metadata_from_fred()
+
+    metadata = pd.concat([
+        metadata_from_fd, metadata_from_investpy, metadata_from_yahoo_main, metadata_from_fred
+    ], axis=0).drop_duplicates(subset='symbol_pk', keep='first') # 40개 가량 중복
+
+    dirpath = new_config.DIR_METADATA_CHUNK
+    os.makedirs(dirpath, exist_ok=True)
+    fname = 'metdata_indices.csv'
+    fpath = os.path.join(dirpath, fname)
+    metadata.to_csv(fpath, index=False)
+    
+
+def etl_metadata_currency():
+    currency = Currency()
+    metadata = currency.get_metadata()
+
+    dirpath = new_config.DIR_METADATA_CHUNK
+    os.makedirs(dirpath, exist_ok=True)
+    fname = 'metadata_currency.csv'
+    fpath = os.path.join(dirpath, fname)
+    metadata.to_csv(fpath, index=False)
+
+
+def etl_history():
+    etf = ETF()
+    indices = Indices()
+    currency = Currency()
+    history = History()
+
+    etf_symbols = etf.get_symbols()
+    currency_symbols = currency.get_symbols()
+    indices_symbols_from_yahoo_main = indices.get_symbols_from_yahoo_main()
+    indices_symbols_from_fd = indices.get_symbols_from_fd()
+    indices_symbols_from_investpy = indices.get_symbols_from_investpy()
+    indices_symbols_yf = indices_symbols_from_yahoo_main + indices_symbols_from_fd + indices_symbols_from_investpy
+    indices_symbols_fred = indices.get_symbols_from_fred()
+    
+    dirpath_history_etf = new_config.DIR_HISTORY_ETF
+    dirpath_history_currency = new_config.DIR_HISTORY_CURRENCY
+    dirpath_history_indices = new_config.DIR_HISTORY_INDICES
+    os.makedirs(dirpath_history_etf, exist_ok=True)
+    os.makedirs(dirpath_history_currency, exist_ok=True)
+    os.makedirs(dirpath_history_indices, exist_ok=True)
+    
+    for symbol in tqdm(etf_symbols[1900:], mininterval=0.5):
+        print(symbol)
+        df = history.transform_history(history.get_history_from_yf(symbol=symbol))
+        if df is not None:
+            fname = f'history_{symbol}.csv'
+            fpath = os.path.join(dirpath_history_etf, fname)
+            df.to_csv(fpath, index=False)
+    
+    for symbol in tqdm(currency_symbols, mininterval=0.5):
+        df = history.transform_history(history.get_history_from_yf(symbol=symbol))
+        if df is not None:
+            fname = f'history_{symbol}.csv'
+            fpath = os.path.join(dirpath_history_currency, fname)
+            df.to_csv(fpath, index=False)
+    
+    for symbol in tqdm(indices_symbols_yf, mininterval=0.5):
+        df = history.transform_history(history.get_history_from_yf(symbol=symbol))
+        if df is not None:
+            fname = f'history_{symbol}.csv'
+            fpath = os.path.join(dirpath_history_indices, fname)
+            df.to_csv(fpath, index=False)
+    
+    for symbol in tqdm(indices_symbols_fred, mininterval=0.5):
+        df = history.transform_history(history.get_history_from_fred(symbol=symbol))
+        if df is not None:
+            fname = f'history_{symbol}.csv'
+            fpath = os.path.join(dirpath_history_indices, fname)
+            df.to_csv(fpath, index=False)
+    
+    # save df by chunk
+    dirpath_history_chunk = new_config.DIR_HISTORY_CHUNK
+    save_dfs_by_chunk(dirpath_history_etf, dirpath_history_chunk, prefix_chunk="concatenated_history_etf")
+    save_dfs_by_chunk(dirpath_history_currency, dirpath_history_chunk, prefix_chunk="concatenated_history_currency")
+    save_dfs_by_chunk(dirpath_history_indices, dirpath_history_chunk, prefix_chunk="concatenated_history_indices")
+
+
+def etl_datedim():
+    datedim = DateDim()
+    df = datedim.get_date_dim()
+
+    dirpath = new_config.DIR_DATEDIM
+    os.makedirs(dirpath, exist_ok=True)
+    fname = 'date_dim.csv'
+    fpath = os.path.join(dirpath, fname)
+    df.to_csv(fpath, index=False)
+
+
+if __name__ == '__main__':
+    # etl_metadata_etf()
+    # etl_metadata_currency()
+    # etl_metadata_indices()
+    # etl_datedim()
+    etl_history()
---
+++ new_utils.py
@@ -0,0 +1,86 @@
+import os
+import numpy as np
+import pandas as pd
+# from tqdm.notebook import tqdm
+from tqdm import tqdm
+from datetime import datetime
+
+def concat_csv_files_in_dir(get_dirpath):
+    df = []
+    csv_file_generator = (pd.read_csv(os.path.join(get_dirpath, csv_fname)) for csv_fname in os.listdir(get_dirpath) if csv_fname.endswith('csv'))
+    for csv_file in tqdm(csv_file_generator, mininterval=0.5, total=len(os.listdir(get_dirpath))):
+        df.append(csv_file)
+    df = pd.concat(df, ignore_index=True)
+    return df
+
+def save_dfs_by_chunk(get_dirpath, put_dirpath, prefix_chunk): # n행씩 분할저장.. # 마지막거는 어케하지..?
+    os.makedirs(put_dirpath, exist_ok=True)
+    df_list = []
+    total_len = 0
+    chunk_num = 1
+
+    df_generator =  (pd.read_csv(os.path.join(get_dirpath, fname)) for fname in os.listdir(get_dirpath) if fname.endswith('csv'))
+    for df in df_generator:
+        df_list.append(df)
+        total_len += len(df)
+        print(f'Concatenateing Dfs in {get_dirpath} | Chunk No.{chunk_num} | Total Length: {total_len}')
+        
+        if total_len > 1000_000:
+            df_concatenated = pd.concat(df_list)
+            fpath = os.path.join(put_dirpath, f'{prefix_chunk}_{chunk_num}.csv')
+            df_concatenated.to_csv(fpath, index=False)
+
+            df_list = []
+            total_len = 0
+            chunk_num += 1
+    
+    if total_len > 0: # 나눠 떨어지지 않은 마지막 사이클 저장
+        df_concatenated = pd.concat(df_list)
+        fpath = os.path.join(put_dirpath, f'{prefix_chunk}_{chunk_num}.csv')
+        df_concatenated.to_csv(fpath, index=False)
+
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = datetime.now()
+        str_start_time = start_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
+        print(f"Process [{func.__name__}] Started at : {str_start_time}")
+
+        result = func(*args, **kwargs)
+        
+        end_time = datetime.now()
+        str_end_time = end_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
+        interval = end_time - start_time
+        str_interval = str(np.round(interval.total_seconds(), 3))
+        print(f"Process [{func.__name__}] Ended at   : {str_end_time}")
+        print(f"Process [{func.__name__}] Runned for : {str_interval} sec")
+        print()
+
+        return result
+    return wrapper
+
+def get_memory_usage(df):
+    # compare with df.info(memory_usage='deep')
+    if isinstance(df, pd.DataFrame):
+        mem = df.memory_usage(deep=True).sum()/(1024**2)
+    elif isinstance(df, pd.Series):
+        mem = df.memory_usage(deep=True)/(1024**2)
+    return(str(round(mem,3))+" MB" )
+
+
+def downcast_df(df, apply_int=True, apply_float=True, apply_string=False, print_size=True):
+    before = get_memory_usage(df)
+    df_copied = df.copy(deep=True)
+    if apply_int:
+        df_int = df_copied.select_dtypes(include=['int']).apply(pd.to_numeric, downcast='integer')
+        df_copied.loc[:,df_int.columns] = df_int
+    if apply_float:
+        df_float = df_copied.select_dtypes(include=['float']).apply(pd.to_numeric, downcast='float')
+        df_copied.loc[:,df_float.columns] = df_float
+    if apply_string:
+        df_string = df_copied.select_dtypes(include=['object']).astype('category')
+        df_copied.loc[:,df_string.columns] = df_string
+    after = get_memory_usage(df_copied)
+    if print_size:
+        print(f"DataFrame Downcasted: {before} -> {after}")
+    return df_copied
