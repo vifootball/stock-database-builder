46facf668977bd73787c9a16dcf6b78cf21b5021
Chung Dongwook <chungdongwook@MacBook-Pro.local>
2023-11-25 22:20:41 +0900

    [feat] main()

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ from bigquery_schema import Schema
 from bigquery_helper import *
 
 # load symbols
-etf_master = pd.read_csv(os.path.join('downloads', 'masters_etf.csv'))
-etf_symbols = list(enumerate(etf_master['symbol'].to_list()))
+# Ray에서 동시 실행 시 에러가 나는 것을 방지하기 위해 enumerate로 감싸주고 순차적으로 데이터를 수집하도록 함
 
-currency_master = pd.read_csv(os.path.join('downloads', 'masters_currency.csv'))
+etf_master = pd.read_csv(os.path.join('downloads', 'master_symbols', 'masters_etf.csv'))
+etf_symbols = etf_master['symbol'].to_list()
+etf_symbols = [x for x in etf_symbols if x not in Etfs.EXCLUDE]
+etf_symbols = list(enumerate(etf_symbols))
+
+currency_master = pd.read_csv(os.path.join('downloads', 'master_symbols', 'masters_currency.csv'))
 currency_symbols = list(enumerate(currency_master['symbol'].to_list()))
 
 indices_master_yahoo = pd.read_csv(os.path.join('downloads', 'masters_indices', 'masters_indices_yahoo.csv'))
@@ -22,6 +26,9 @@ indices_symbols_yahoo = list(enumerate(indices_master_yahoo['symbol'].to_list())
 
 indices_master_investpy = pd.read_csv(os.path.join('downloads', 'masters_indices', 'masters_indices_investpy.csv'))
 indices_symbols_investpy = list(enumerate(indices_master_investpy['symbol'].to_list()))
+
+indices_master_fd = pd.read_csv(os.path.join('downloads', 'masters_indices', 'masters_indices_fd.csv'))
+indices_symbols_fd = list(enumerate(indices_master_investpy['symbol'].to_list()))
 
 indices_master_fred = pd.read_csv(os.path.join('downloads', 'masters_indices', 'masters_indices_fred.csv'))
 indices_symbols_fred = list(enumerate(indices_master_fred['symbol'].to_list()))
@@ -70,56 +77,75 @@ def collect_indices_history_from_fred(order, symbol):
 
 if __name__ == '__main__':
 
-    ### History
-    # print(etf_symbols[:10])
-    # print(indices_symbols_fred)
+    ### Collect History
+    ray.init(ignore_reinit_error=True,  num_cpus=8)
+    
+    tasks_collect_etf_history_from_yf = [collect_etf_history_from_yf.remote(order, symbol) for order, symbol in etf_symbols[:]]
+    ray.get(tasks_collect_etf_history_from_yf)
+    
+    tasks_collect_currency_history_from_yf = [collect_currency_history_from_yf.remote(order, symbol) for order, symbol in currency_symbols[:]]
+    ray.get(tasks_collect_currency_history_from_yf)
 
-    # ray.init(ignore_reinit_error=True,  num_cpus=8)
+    tasks_collect_indices_investpy_history_from_yf = [collect_indices_history_from_yf.remote(order, symbol) for order, symbol in indices_symbols_investpy[:]]
+    ray.get(tasks_collect_indices_investpy_history_from_yf)
     
-    # tasks_collect_etf_history_from_yf = [collect_etf_history_from_yf.remote(order, symbol) for order, symbol in etf_symbols[:]]
-    # ray.get(tasks_collect_etf_history_from_yf)
+    tasks_collect_indices_yahoo_history_from_yf = [collect_indices_history_from_yf.remote(order, symbol) for order, symbol in indices_symbols_yahoo[:]]
+    ray.get(tasks_collect_indices_yahoo_history_from_yf)
     
-    # tasks_collect_currency_history_from_yf = [collect_currency_history_from_yf.remote(order, symbol) for order, symbol in currency_symbols[:]]
-    # ray.get(tasks_collect_currency_history_from_yf)
-
-    # tasks_collect_indices_investpy_history_from_yf = [collect_indices_history_from_yf.remote(order, symbol) for order, symbol in indices_symbols_investpy[:]]
-    # ray.get(tasks_collect_indices_investpy_history_from_yf)
-    
-    # tasks_collect_indices_yahoo_history_from_yf = [collect_indices_history_from_yf.remote(order, symbol) for order, symbol in indices_symbols_yahoo[:]]
-    # ray.get(tasks_collect_indices_yahoo_history_from_yf)
-    
-    # tasks_collect_indices_history_from_fred = [collect_indices_history_from_fred.remote(order,symbol) for order, symbol in indices_symbols_fred[:]]
-    # ray.get(tasks_collect_indices_history_from_fred)
+    tasks_collect_indices_history_from_fred = [collect_indices_history_from_fred.remote(order,symbol) for order, symbol in indices_symbols_fred[:]]
+    ray.get(tasks_collect_indices_history_from_fred)
 
     ### Make History Chunk
     save_dfs_by_chunk(
         get_dirpath="./downloads/history/currency/",
-        put_dirpath="./downloads/history/chunks/",
+        put_dirpath="./downloads/bigquery_tables/history_chunks/",
         prefix_chunk="currency_chunk"
     )
+    save_dfs_by_chunk(
+        get_dirpath="./downloads/history/etf/",
+        put_dirpath="./downloads/bigquery_tables/history_chunks/",
+        prefix_chunk="etf_chunk"
+    )
+    save_dfs_by_chunk(
+        get_dirpath="./downloads/history/indices/",
+        put_dirpath="./downloads/bigquery_tables/history_chunks/",
+        prefix_chunk="indices_chunk"
+    )
 
-    ### Summary Grade
-    run_summary_main()
+    ### Calculate Summary Grade
+    run_summary_grade_main()
+
 
     ### Bigquery Upload
     bq_project_id = "between-buy-and-sell"
     bq_dataset_id ="stock"
     schema = Schema()
-    
-    print()
+
+    # HISTORY    
     copy_local_csv_files_to_bq_table(
-        local_dirpath='./downloads/bigquery_tables',
+        local_dirpath='./downloads/bigquery_tables/history_chunks/',
+        bq_project_id=bq_project_id, bq_dataset_id=bq_dataset_id, bq_table_name='history',
+        schema=schema.HISTORY
+    )
+    
+    # DM_GRADE
+    copy_local_csv_file_to_bq_table(
+        local_fpath='./downloads/bigquery_tables/summary_grades.csv',
+        bq_project_id=bq_project_id, bq_dataset_id=bq_dataset_id, bq_table_name='dm_grade',
+        schema=schema.DM_GRADE
+    )
+
+    # DM_GRADE_PIVOT
+    copy_local_csv_file_to_bq_table(
+        local_fpath='./downloads/bigquery_tables/summary_grades_piv.csv',
+        bq_project_id=bq_project_id, bq_dataset_id=bq_dataset_id, bq_table_name='dm_grade_pivot',
+        schema=schema.DM_GRADE_PIVOT
+    )
+
+    copy_local_csv_file_to_bq_table(
+        local_fpath='./downloads/bigquery_tables/master_date.csv',
         bq_project_id=bq_project_id, bq_dataset_id=bq_dataset_id, bq_table_name="master_date",
         schema=schema.MASTER_DATE
     )
-
-
-
-
-
-
-#     bq_uploader = BqUploader()
-#     # bq_uploader.upload_summaries_to_bq()
-#     bq_uploader.upload_history_to_bq()
 
 #     print("Done")
